0cc83ebdad48bda09098b80974ca0a90f1514f82
TsuguhikoOkada <tsugu.pc.academy@gmail.com>
2024-04-21 08:46:58 +0900

    Added SoundManager.cs, Title and CreditScene.

---
+++ FishTank/Assets/Scripts/Tsuguhiko/SoundManager.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Manages sound playback throughout the game, including music and sound effects.
+/// </summary>
+public class SoundManager : MonoBehaviour
+{
+    /// <summary>
+    /// AudioSource used for background music playback.
+    /// </summary>
+    public AudioSource musicSource;
+
+    /// <summary>
+    /// AudioSource used for playing sound effects.
+    /// </summary>
+    public AudioSource soundEffectSource;
+
+    /// <summary>
+    /// An array of AudioClip, each representing a different music track for various game scenes.
+    /// </summary>
+    public AudioClip[] musicTracks;
+
+    /// <summary>
+    /// An array of AudioClip for different sound effects in the game.
+    /// </summary>
+    public AudioClip[] soundEffects;
+
+    private static SoundManager instance = null;
+
+    /// <summary>
+    /// Singleton instance of the SoundManager to ensure it can be accessed globally without duplicates.
+    /// </summary>
+    public static SoundManager Instance => instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            musicSource.loop = true; // Ensures that the music loops continuously.
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    /// <summary>
+    /// Plays a sound effect from the soundEffects array at the specified index.
+    /// </summary>
+    /// <param name="index">Index of the sound effect to play in the soundEffects array.</param>
+    public void PlaySoundEffect(int index)
+    {
+        if (index >= 0 && index < soundEffects.Length)
+        {
+            soundEffectSource.PlayOneShot(soundEffects[index]);
+        }
+    }
+
+    /// <summary>
+    /// Plays background music from the musicTracks array at the specified index.
+    /// If music is already playing, the function will not attempt to play another track.
+    /// </summary>
+    /// <param name="index">Index of the music track to play in the musicTracks array.</param>
+    public void PlayMusic(int index)
+    {
+        if (index >= 0 && index < musicTracks.Length && !musicSource.isPlaying)
+        {
+            musicSource.clip = musicTracks[index];
+            musicSource.Play();
+        }
+    }
+
+    /// <summary>
+    /// Stops the currently playing music.
+    /// </summary>
+    public void StopMusic()
+    {
+        if (musicSource.isPlaying)
+        {
+            musicSource.Stop();
+        }
+    }
+
+    /// <summary>
+    /// Sets the volume of the background music.
+    /// </summary>
+    /// <param name="volume">Volume level to set, ranging from 0.0 to 1.0.</param>
+    public void SetMusicVolume(float volume)
+    {
+        musicSource.volume = volume;
+    }
+}
---
+++ FishTank/Assets/Scripts/Tsuguhiko/Title.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI; // Added to use the Image component
+using DG.Tweening; // Added to use DoTween
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+/// <summary>
+/// Class that provides functionality for the title screen.
+/// </summary>
+public class Title : MonoBehaviour
+{
+    [SerializeField]Image panel; // Attach the Image component of the Panel
+    [SerializeField] private float fadeDuration = 0.5f; // Duration of the fade out effect, adjustable in the inspector
+
+    private void Start()
+    {
+        // Ensure the panel is disabled at the start
+        panel.gameObject.SetActive(false);
+    }
+
+    /// <summary>
+    /// Transitions to the main game scene with a fade out effect.
+    /// </summary>
+    public void StartGame()
+    {
+        FadeOut(() => SceneManager.LoadScene("MainScene"));
+    }
+
+    /// <summary>
+    /// Transitions to the credits scene with a fade out effect.
+    /// </summary>
+    public void OpenCredits()
+    {
+        FadeOut(() => SceneManager.LoadScene("Credit Scene"));
+    }
+
+    /// <summary>
+    /// Exits the game with a fade out effect.
+    /// </summary>
+    public void ExitGame()
+    {
+        FadeOut(() =>
+        {
+#if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        });
+    }
+
+    /// <summary>
+    /// Fades out the screen and calls the provided action after the animation.
+    /// </summary>
+    /// <param name="onComplete">Action to call after the fade out completes.</param>
+    private void FadeOut(System.Action onComplete)
+    {
+        // Ensure the panel is visible before starting the fade
+        panel.gameObject.SetActive(true);
+        panel.color = new Color(panel.color.r, panel.color.g, panel.color.b, 0); // Start from transparent
+        panel.DOFade(1, fadeDuration) // Fade to opaque over specified duration
+            .OnComplete(() => onComplete()); // Call onComplete after the animation completes
+    }
+}
