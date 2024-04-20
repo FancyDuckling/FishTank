a30acc651145cacc351641d6691871bff88fbb7a
Waterhuman33 <vide@eneljung.se>
2024-04-20 19:26:41 +0200

    Spinning fish

--- FishTank/Assets/Scripts/Vide/OverallHealth.cs
+++ FishTank/Assets/Scripts/Vide/OverallHealth.cs
@@ -9,14 +9,96 @@ public class OverallHealth : MonoBehaviour
     public Slider tempBar;
     public Material material;
     public Color[] colors;
+    private int idealTemp;
+    int initialValue;
+    int lastIndex;
+    bool firstTime = true;
+    int timer = 0;
     void Start()
     {
+        idealTemp = (int)Random.Range(10, tempBar.maxValue);
+        Debug.Log(idealTemp);
+        StartCoroutine(Timer());
         
     }
-
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(0.5f);
+        UpdateOverallHealth();
+        StartCoroutine(Timer());
+    }
+  
     // Update is called once per frame
     public void UpdateOverallHealth()
     {
-            
+        var diff = idealTemp - tempBar.value;
+        Debug.Log("Diff Value : "+ diff);
+        if( diff == 0 )
+        {
+            TransitionColor(10, 3);   
+        }
+        else if ( Mathf.Abs(diff) < 5)
+        {
+            TransitionColor(5, 3);
+        }
+        else if (Mathf.Abs(diff) < 10)
+        {
+            TransitionColor(1,2);
+        }
+        else if (Mathf.Abs(diff) < 15)
+        {
+            TransitionColor(-5, 1);
+        }
+        else
+        {
+            TransitionColor(-10, 1);
+        }
+
     }
-}
+    void TransitionColor(int addedValue, int index)
+    {
+        var newValue = initialValue + addedValue;
+        
+        
+        //if (newValue > 75)
+        //{
+        //    index = 4;
+        //}
+        //else if (newValue > 50)
+        //{
+        //    index = 3;
+        //}
+        //else if (newValue > 25)
+        //{
+        //    index = 2;
+        //}
+        //else
+        //{
+        //    index = 1;
+        //}
+        if (firstTime == false)
+        {
+            material.color = Color.Lerp(colors[lastIndex], colors[index], initialValue/(index * 25));
+            lastIndex = index;
+
+        }
+        else
+        {
+            material.color = Color.Lerp(colors[0], colors[index], 1);
+            firstTime = false;
+            lastIndex = index;
+        }
+
+       
+        initialValue = newValue;
+        initialValue = Mathf.Clamp(initialValue, 0, 100);
+        if (initialValue == 100) 
+        {
+            material.color = colors[4];
+            idealTemp = (int)Random.Range(10, tempBar.maxValue);
+
+        }
+        Debug.Log("CurrentColor " + initialValue);
+    }
+
+    }
---
+++ FishTank/Assets/Scripts/Vide/SpinDoTween2.cs
@@ -0,0 +1,26 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class SpinDoTween2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        transform.DORotate(new Vector3(360, 0, 0), 2.5f, RotateMode.FastBeyond360).SetRelative(true).SetLoops(-1, LoopType.Incremental).SetEase(Ease.Linear);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void SwimAnim()
+    {
+        animator.Play(0);
+    }
+}
