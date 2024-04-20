a7888f6b5c6b27888e5c296d314727376e78f780
FancyDuckling <FancyDuckling@users.noreply.github.com>
2024-04-20 19:24:54 +0200

    poop placement

--- FishTank/Assets/Scripts/PoopWater.cs
+++ FishTank/Assets/Scripts/PoopWater.cs
@@ -11,12 +11,15 @@ public class PoopWater : MonoBehaviour
     public bool fishIsFull = false;
     public GameObject poop;
     public GameObject fish;
+    public GameObject cleaningEquipment;
     
     private int curentColorIndex = 0;
     private int targetColorIndex = 1;
     private float targetPoint;
+
     
-   void Start()
+
+    void Start()
     {
         tankIsClean = true;
     }
@@ -24,9 +27,10 @@ public class PoopWater : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.E))
+        if (Input.GetKey(KeyCode.E)) //Clean the aquarium
         {
             tankIsClean = true;
+            
 
         }
 
@@ -47,22 +51,6 @@ public class PoopWater : MonoBehaviour
             TransitionToClean();
         }
        
-        
-            
-        
-        
-        
-        
-        /*TransitionToClean();
-
-
-        if (fishIsFull)
-            TransitionToDirty();
-        else
-            targetColorIndex = 0;*/
-
-
-
 
     }
 
@@ -70,39 +58,26 @@ public class PoopWater : MonoBehaviour
     {
         targetPoint += Time.deltaTime/time;
         material.color = Color.Lerp(colors[curentColorIndex], colors[targetColorIndex],targetPoint);
-
+       // PlacePoop();
         if (targetPoint >= 1f)
         {
+            PlacePoop();
             targetPoint = 0f;
             curentColorIndex = targetColorIndex;
             targetColorIndex++;
-            PlacePoop();
+            
             if (targetColorIndex >= colors.Length)
             {
                 targetColorIndex = 3;
             }
         }
 
-        /*if (targetPoint >= 1f)
-        {
-            targetPoint = 0f;
-            curentColorIndex = targetColorIndex;
-            targetColorIndex++;
-            if (targetColorIndex == colors.Length)
-            {
-                targetColorIndex = 3;
-                
-                
-            }
-            
-           
-                
-        }*/
+        
     }
 
     void TransitionToClean()
     {
-        //material.color = colors[0];
+        
 
         targetPoint += Time.deltaTime / time;
         material.color = Color.Lerp(colors[curentColorIndex], colors[0], targetPoint);
@@ -112,7 +87,9 @@ public class PoopWater : MonoBehaviour
             targetPoint = 0f;
             curentColorIndex = 0;
             targetColorIndex = 1;
+
             
+
         }
 
 
@@ -121,19 +98,20 @@ public class PoopWater : MonoBehaviour
 
     void PlacePoop()
     {
-        // Instantiate two poop objects
-        GameObject poop1 = Instantiate(poop, transform.position, Quaternion.identity);
-        GameObject poop2 = Instantiate(poop, transform.position, Quaternion.identity);
+        
+        GameObject poops = Instantiate(poop, fish.transform.position, Quaternion.identity);
+        
 
-       
-        Vector3 offset1 = new Vector3(Random.Range(-1f, 1f), 0f, Random.Range(-1f, 1f));
-        Vector3 offset2 = new Vector3(Random.Range(-1f, 1f), 0f, Random.Range(-1f, 1f));
-        poop1.transform.position += offset1;
-        poop2.transform.position += offset2;
     }
 
-    void CleanPoop()
-    {
 
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("CleaningEquipment"))
+        {
+            Destroy(gameObject);
+        }
+            
+        
     }
 }
