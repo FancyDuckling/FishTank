30b61eba99efd863faf49ddf8e8e3216d2707780
TsuguhikoOkada <tsugu.pc.academy@gmail.com>
2024-04-21 22:03:06 +0900

    Fixed Temperature.cs .

--- FishTank/Assets/Scripts/Vide/Temperature.cs
+++ FishTank/Assets/Scripts/Vide/Temperature.cs
@@ -10,38 +10,34 @@ public class Temperature : MonoBehaviour
     public Slider tempMeterSlider;
     public TextMeshProUGUI text;
     private float lastMeasuredTemp;
+
     void Start()
     {
         tempMeterSlider.value = Random.Range(0, 51);
         text.text = tempMeterSlider.value.ToString();
     }
 
-
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             ChangeTemperature(+1);
-
-            //Play UI Temp UP
-            
-            
+            // Play sound effect for temperature increase
+            SoundManager.Instance.PlaySoundEffect(2); // Adjust the index based on your sound effects array
         }
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             ChangeTemperature(-1);
-
-            //Play UI Temp Down
-            
+            // Play sound effect for temperature decrease
+            SoundManager.Instance.PlaySoundEffect(1); // Adjust the index based on your sound effects array
         }
     }
-    public void ChangeTemperature(float value) 
+
+    public void ChangeTemperature(float value)
     {
         tempMeterSlider.value += value;
         tempMeter.fillAmount = tempMeterSlider.value / tempMeterSlider.maxValue;
         text.text = tempMeterSlider.value.ToString();
         Debug.Log("Fill Image");
-        ;
     }
 }
