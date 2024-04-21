9207f8f449f397cff58835051353050e994e8e35
FancyDuckling <FancyDuckling@users.noreply.github.com>
2024-04-21 22:44:24 +0200

    fixed temprature script

--- FishTank/Assets/Scripts/Vide/Temperature.cs
+++ FishTank/Assets/Scripts/Vide/Temperature.cs
@@ -10,19 +10,14 @@ public class Temperature : MonoBehaviour
     public Slider tempMeterSlider;
     public TextMeshProUGUI text;
     private float lastMeasuredTemp;
-<<<<<<< HEAD
-    // Reference to the SoundManager
-    private SoundManager soundManager;
-=======
->>>>>>> 30b61eba99efd863faf49ddf8e8e3216d2707780
+
 
     void Start()
     {
         tempMeterSlider.value = Random.Range(0, 51);
         text.text = tempMeterSlider.value.ToString();
 
-        // Get reference to the SoundManager instance
-        soundManager = SoundManager.Instance;
+        
     }
 
     void Update()
@@ -30,37 +25,12 @@ public class Temperature : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             ChangeTemperature(+1);
-<<<<<<< HEAD
 
-            //Play UI Temp UP
-            // Play sound effect for temperature increase
-            if (soundManager != null)
-            {
-                soundManager.PlaySoundEffect(2); // Adjust the index based on your sound effects array
-            }
-
-
-=======
-            // Play sound effect for temperature increase
-            SoundManager.Instance.PlaySoundEffect(2); // Adjust the index based on your sound effects array
->>>>>>> 30b61eba99efd863faf49ddf8e8e3216d2707780
         }
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             ChangeTemperature(-1);
-<<<<<<< HEAD
 
-            //Play UI Temp Down
-            // Play sound effect for temperature decrease
-            if (soundManager != null)
-            {
-                soundManager.PlaySoundEffect(1); // Adjust the index based on your sound effects array
-            }
-
-=======
-            // Play sound effect for temperature decrease
-            SoundManager.Instance.PlaySoundEffect(1); // Adjust the index based on your sound effects array
->>>>>>> 30b61eba99efd863faf49ddf8e8e3216d2707780
         }
     }
 
